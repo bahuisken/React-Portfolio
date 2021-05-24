89e27332c5ffd6dd606afe31f8915290640971c2
bahuisken <brice.huisken@gmail.com>
2021-05-23 23:45:24 -0600

    Trying to dfeploy

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const APIURL =
-  "https://randomuser.me/api/?results=100&nat=us&inc=picture,name,email,phone,dob,login";
-// eslint-disable-next-line
-export default {
-  search: function (query) {
-    return axios.get(APIURL);
-  },
-};
